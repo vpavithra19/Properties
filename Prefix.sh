8a5b9a3fcd004489b5d05995670775312b876270
lakshmipavithra <lakshmipavithra194@gmail.com>
2021-05-12 18:04:22 +0530

    yes 1

---
+++ Prefix.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PREFIX="$1_"
+
+keys=($(awk -F= '{print $1}' input.properties))
+properties=($(awk -F= '{print $2}' input.properties))
+
+count=${#keys[@]}
+
+for ((i=0; i<$count; i++)); do
+   key=${keys[$i]}
+   property=${properties[$i]}
+
+   if [[ $key == $PREFIX* ]]; # True if $key starts with a $PREFIX (wildcard matching).
+   then
+      originalKey=${key/$PREFIX/} # Remove prefix
+      searchValue="${originalKey}[[:blank:]]*=[[:blank:]]*.*"
+      replaceValue="$originalKey=$property"
+
+      sed -i -e "s/^$searchValue/$replaceValue/" out.properties
+   fi
+done
